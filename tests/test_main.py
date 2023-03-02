4fcc875b9490cb7eebafc6ee620aba105b55644c
Ser <ivanov.kir2001@gmail.com>
2023-03-02 15:15:26 +0300

    test4

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,9 @@
 import requests
 
-api_url="http://localhost:8000"
+api_url="http://127.0.0.1:8000"
 
 def test_healthCheck():
     response = requests.get(f'{api_url}/__health_check')
     assert response.status_code == 200
+
+# class TestDociments():
