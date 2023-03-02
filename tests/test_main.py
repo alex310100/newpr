007b9758fb8467b758b13c1906a69294a6b392b3
Ser <ivanov.kir2001@gmail.com>
2023-03-02 16:21:54 +0300

    update adres

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,13 +9,13 @@ def test_healthCheck():
 class TestDociments():
 
     def test_empty_get_Docs(self):
-        response = requests.get(f'{api_url}/doc/docs')
+        response = requests.get("http://localhost:8000/doc/docs")
         assert response.status_code == 200
         assert len(response.json()) == 0
 
     def test_post_Doc(self):
         body = {"title": "shittitle", "body": "shitBody"}
-        response = requests.post(f'{api_url}/doc/docs',json=body)
+        response = requests.post("http://localhost:8000/doc/docs",json=body)
         assert response.status_code == 200
         assert response.json().get('title') == "shittitle"
 
