a1cd1c7f0a7e8817d950a2886e2fbca11a0e4083
Ser <ivanov.kir2001@gmail.com>
2023-03-02 15:48:32 +0300

    add new test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,33 @@
 import requests
 
-api_url="http://127.0.0.1:8000"
+api_url="http://localhost:8000"
 
 def test_healthCheck():
     response = requests.get(f'{api_url}/__health_check')
     assert response.status_code == 200
 
-# class TestDociments():
+class TestDociments():
+
+    def test_empty_get_Docs(self):
+        response = requests.get(f'{api_url}/doc/docs')
+        assert response.status_code == 200
+        assert len(response.json()) == 0
+
+    def test_post_Doc(self):
+        body = {"title": "shittitle", "body": "shitBody"}
+        response = requests.post(f'{api_url}/doc/docs',json=body)
+        assert response.status_code == 200
+        assert response.json().get('title') == "shittitle"
+        assert len(response.json()) == 1
+
+    def test_get_id_Doc(self):
+        response = requests.get(f'{api_url}/doc/docs/0')
+        assert response.status_code == 200
+        assert response.json().get('title') == "shittitle"
+        assert len(response.json()) == 1
+
+    def test_fill_get_Docs(self):
+        response = requests.get(f'{api_url}/doc/docs')
+        assert response.status_code == 200
+        assert len(response.json()) == 1
+
